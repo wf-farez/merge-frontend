554ca6114d6365c10fc849103368a32426c4aae6
robinson-arpi <robinson.arpi@gmail.com>
2024-08-13 23:45:47 -0500

    add structure for our team

--- src/app/components/our-team/our-team.component.ts
+++ src/app/components/our-team/our-team.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { SimpleCardComponent } from "../shared/simple-card/simple-card.component";
 
 @Component({
   selector: 'app-our-team',
   standalone: true,
-  imports: [CardModule],
+  imports: [CardModule, SimpleCardComponent],
   templateUrl: './our-team.component.html',
   styleUrl: './our-team.component.scss'
 })
