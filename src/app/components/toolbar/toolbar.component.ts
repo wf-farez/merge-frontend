5c80b10fe9625353baec524bb1d562a01ba8190b
RolandoDev <rolando.mizhquiri@gmail.com>
2024-08-13 20:31:46 -0500

    adding changes

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
 import { AvatarModule } from 'primeng/avatar';
 import "primeicons/primeicons.css";
-
+import {ButtonModule} from 'primeng/button';
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [AvatarModule,ToolbarModule ],
+  imports: [AvatarModule,ToolbarModule, ButtonModule ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
