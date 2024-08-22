436eeb194681d088d65ad586c0ce3b823aa20e99
KevinJuela20 <fernando.dev2024@gmail.com>
2024-08-22 09:47:26 -0500

    Color correction and toolbar design. Display of the hamburger menu

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { ToolbarModule } from 'primeng/toolbar';
 import "primeicons/primeicons.css";
 import {ButtonModule} from 'primeng/button';
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [ToolbarModule, ButtonModule ],
+  imports: [ToolbarModule, ButtonModule, CommonModule ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
+  menuVisible: boolean = false;
+
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+    console.log("Menu visibility: ", this.menuVisible);
+  }
 
 }
